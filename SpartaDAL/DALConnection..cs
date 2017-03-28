b04a0ce318b60846d49d01d7861b7bd003ffc75d
Joost96 <joost-k@live.nl>
2017-03-28 11:29:34 +0200

    connection db

---
+++ SpartaDAL/DALConnection..cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparta.Dal
+{
+    public static class DALConnection
+    {
+        public static SqlConnection GetConnectionByName(string naam)
+        {
+            string conString = ConfigurationManager.ConnectionStrings[naam].ConnectionString;
+            return new SqlConnection(conString);
+        }
+    }
+}
